c950e9057ada7cb8c5f6511d4f46a541033150f0
Oliver Giovanni <olivergiovanni97@gmail.com>
2025-06-19 12:04:04 +0700

    fix ringkasan transaksi

--- src/container/SewaArmada/Web/SummaryPanel/SummaryPanel.jsx
+++ src/container/SewaArmada/Web/SummaryPanel/SummaryPanel.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Button from "@/components/Button/Button";
 import Card from "@/components/Card/Card";
