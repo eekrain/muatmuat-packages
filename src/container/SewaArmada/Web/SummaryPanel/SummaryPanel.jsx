92f9834d01f4ce9be74899374a6bfdcbcbce76b2
Andy Windows <andy.johannes07@gmail.com>
2025-07-01 12:01:10 +0700

    fixing state

--- src/container/SewaArmada/Web/SummaryPanel/SummaryPanel.jsx
+++ src/container/SewaArmada/Web/SummaryPanel/SummaryPanel.jsx
@@ -92,7 +92,7 @@ export const SummaryPanel = () => {
   const [isModalConfirmationOpen, setIsModalConfirmationOpen] = useState(false);
   const baseOrderAmount = 950000;
   const [currentTotal, setCurrentTotal] = useState(0);
-
+  const [showInfoPopup, setShowInfoPopup] = useState(null);
   // Method: Using flatMap and reduce
   const detailPesanan = [
     {
