c104c83907a1a406a55885c69f96a5e22ee2d55e
cakradana <rcakradana@gmail.com>
2025-07-15 20:52:35 +0700

    feat: add Transporter layout and page components

---
+++ src/app/transporter/(main)/layout.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import useDevice from "@/hooks/use-device";
+import DesktopLayout from "@/layout/Shipper/DesktopLayout/DesktopLayout";
+
+export default function RootLayout({ children }) {
+  const { mounted } = useDevice();
+
+  if (!mounted) return null;
+
+  return <DesktopLayout>{children}</DesktopLayout>;
+}
