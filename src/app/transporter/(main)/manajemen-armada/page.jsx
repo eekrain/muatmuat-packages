e318dc532e720ad611fd2eeedb783dd1a4071db9
cakradana <rcakradana@gmail.com>
2025-07-16 17:01:46 +0700

    feat: add Tambah Armada and Ubah Armada pages

--- src/app/transporter/(main)/manajemen-armada/page.jsx
+++ src/app/transporter/(main)/manajemen-armada/page.jsx
@@ -1,6 +1,8 @@
 import ArmadaContainer from "@/container/Transporter/Armada/ArmadaContainer";
 
 const Page = () => {
+  // const { data } = getDataArmada();
+
   return (
     <div className="">
       <ArmadaContainer />
