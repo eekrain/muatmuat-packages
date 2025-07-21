91b0a17bff09e5627895fc87a25f49de8a7492e2
cakradana <rcakradana@gmail.com>
2025-07-21 17:32:54 +0700

    Adds optional back button to PageTitle
    Allows hiding the back button in the PageTitle component.
    
    This provides more flexibility when using the component
    in places where a back button is not needed.

--- src/components/PageTitle/PageTitle.jsx
+++ src/components/PageTitle/PageTitle.jsx
@@ -2,25 +2,27 @@ import { useRouter } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 
-const PageTitle = ({ className, href = null, children }) => {
+const PageTitle = ({ className, href = null, children, withBack = true }) => {
   const router = useRouter();
 
   return (
     <div className={cn("mb-4 flex items-center gap-3", className)}>
-      <img
-        src="/icons/arrow-left24.svg"
-        width={24}
-        height={24}
-        className="cursor-pointer"
-        alt="Back"
-        onClick={() => {
-          if (href) {
-            router.push(href);
-          } else {
-            router.back();
-          }
-        }}
-      />
+      {withBack && (
+        <img
+          src="/icons/arrow-left24.svg"
+          width={24}
+          height={24}
+          className="cursor-pointer"
+          alt="Back"
+          onClick={() => {
+            if (href) {
+              router.push(href);
+            } else {
+              router.back();
+            }
+          }}
+        />
+      )}
       <h1 className="capsize text-xl font-bold">{children}</h1>
     </div>
   );
