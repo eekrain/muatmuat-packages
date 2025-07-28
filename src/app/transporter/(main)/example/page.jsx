734ea5779677368d6af9a62d53631e404324cbaf
Jerrie Jayadi <jerriejayadi@gmail.com>
2025-07-28 15:16:42 +0700

    feat: Add example page to transporter

---
+++ src/app/transporter/(main)/example/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import ExampleResponsive from "@/container/Shipper/Example/Responsive/ExampleResponsive";
+import ExampleWeb from "@/container/Shipper/Example/Web/ExampleWeb";
+import useDevice from "@/hooks/use-device";
+
+export default function Page() {
+  const { isMobile } = useDevice();
+
+  if (isMobile) {
+    return <ExampleResponsive />;
+  }
+  return <ExampleWeb />;
+}
