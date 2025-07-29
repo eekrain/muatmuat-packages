272518e56f1d0a37e89047753b1d305884528c40
Fawwazxdam <fawwazadam1005@gmail.com>
2025-07-29 16:46:40 +0700

    feat: enhance cancellation process in BottomsheetAlasanPembatalan and ModalBatalkanPesananResponsive
    - Integrated bank account check in BottomsheetAlasanPembatalan to navigate users to FormRekeningBank if no accounts are available.
    - Updated ModalBatalkanPesananResponsive to conditionally render content based on order status, improving user guidance during cancellation.
    - Refactored modal structure for better readability and maintainability.

--- src/container/Shipper/DetailPesanan/Responsive/Home/components/ModalBatalkanPesananResponsive.jsx
+++ src/container/Shipper/DetailPesanan/Responsive/Home/components/ModalBatalkanPesananResponsive.jsx
@@ -4,16 +4,18 @@ import { useEffect, useState } from "react";
 import Button from "@/components/Button/Button";
 import Checkbox from "@/components/Form/Checkbox";
 import { Modal, ModalContent, ModalTrigger } from "@/components/Modal/Modal";
+import { OrderStatusEnum } from "@/lib/constants/detailpesanan/detailpesanan.enum";
 
 /**
  * @typedef {object} BatalkanPesananModalProps
  * @property {boolean} open - Controls the visibility of the modal.
  * @property {(isOpen: boolean) => void} onOpenChange - Function to call when the modal's open state changes.
  * @property {() => void} onConfirm - Function to call when the confirmation button is clicked.
+ * @property {string} orderStatus - The order status to determine modal content.
  */
 
 /**
- * A confirmation modal for cancelling an order.
+ * A confirmation modal for cancelling an order, responsive version.
  *
  * @param {ModalBatalkanPesananResponsive} props
  */
@@ -22,6 +24,7 @@ export const ModalBatalkanPesananResponsive = ({
   onOpenChange,
   children,
   onConfirm = () => alert("Not implemented"),
+  orderStatus,
 }) => {
   const [isAgreed, setIsAgreed] = useState(false);
 
@@ -40,57 +43,130 @@ export const ModalBatalkanPesananResponsive = ({
     }
   }, [open]);
 
+  // Content based on orderStatus
+  let ContentComponent;
+  if (orderStatus === OrderStatusEnum.PREPARE_FLEET) {
+    ContentComponent = CancelContentWhenPrepareFleet;
+  } else {
+    ContentComponent = CancelContentWhenNotPrepareFleet;
+  }
+
   return (
-    <>
-      <Modal open={open} onOpenChange={handleOpenChange}>
-        <ModalTrigger asChild>{children}</ModalTrigger>
-        <ModalContent className="w-[296px] rounded-[10px] bg-white px-4 py-6 shadow-[0px_2px_20px_rgba(0,0,0,0.25)]">
-          <div className="relative flex flex-col items-center gap-5">
-            <div className="flex flex-col items-center gap-4 text-center">
-              <h2 className="text-base font-bold text-neutral-900">
-                Batalkan Pesanan
-              </h2>
-              <p className="text-sm font-medium text-neutral-900">
-                Apakah kamu yakin ingin membatalkan pesanan?
-                <br />
-                <br />
-                Pastikan kamu sudah membaca{" "}
-                <Link
-                  href="https://faq.muatmuat.com/pusat-bantuan"
-                  className="text-primary-700 underline"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  onClick={(e) => e.stopPropagation()}
-                >
-                  Syarat dan Ketentuan kami.
-                </Link>
-              </p>
-            </div>
-
-            <Checkbox
-              checked={isAgreed}
-              onChange={({ checked }) => setIsAgreed(checked)}
-              className="w-full"
-            >
-              <span className="text-sm font-semibold text-neutral-900">
-                Ya, Saya setuju dengan syarat dan ketentuan tersebut
-              </span>
-            </Checkbox>
-
-            <Button
-              variant="muatparts-primary-secondary"
-              className="w-[146px]"
-              disabled={!isAgreed}
-              onClick={() => {
-                onConfirm();
-                onOpenChange(false);
-              }}
-            >
-              Batalkan Pesanan
-            </Button>
-          </div>
-        </ModalContent>
-      </Modal>
-    </>
+    <Modal open={open} onOpenChange={handleOpenChange}>
+      <ModalTrigger asChild>{children}</ModalTrigger>
+      <ModalContent className="w-[296px] rounded-[10px] bg-white px-4 py-6 shadow-[0px_2px_20px_rgba(0,0,0,0.25)]">
+        <ContentComponent
+          isAgreed={isAgreed}
+          setIsAgreed={setIsAgreed}
+          onConfirm={onConfirm}
+          onOpenChange={onOpenChange}
+        />
+      </ModalContent>
+    </Modal>
   );
 };
+
+const CancelContentWhenPrepareFleet = ({
+  isAgreed,
+  setIsAgreed,
+  onConfirm,
+  onOpenChange,
+}) => (
+  <div className="relative flex flex-col items-center gap-5">
+    <div className="flex flex-col items-center gap-4 text-center">
+      <h2 className="text-base font-bold text-neutral-900">Batalkan Pesanan</h2>
+      <p className="text-sm font-medium text-neutral-900">
+        Apakah kamu yakin ingin membatalkan pesanan?
+        <br />
+        <br />
+        Pastikan kamu sudah membaca{" "}
+        <Link
+          href="https://faq.muatmuat.com/pusat-bantuan"
+          className="text-primary-700 underline"
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={(e) => e.stopPropagation()}
+        >
+          Syarat dan Ketentuan Muatrans
+        </Link>
+      </p>
+    </div>
+    <Checkbox
+      checked={isAgreed}
+      onChange={({ checked }) => setIsAgreed(checked)}
+      className="w-full"
+    >
+      <span className="text-sm font-semibold text-neutral-900">
+        Saya menyetujui{" "}
+        <Link
+          href="https://faq.muatmuat.com/pusat-bantuan"
+          className="text-primary-700 underline"
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={(e) => e.stopPropagation()}
+        >
+          Syarat dan Ketentuan Muatrans
+        </Link>
+      </span>
+    </Checkbox>
+    <Button
+      variant="muatparts-primary-secondary"
+      className="w-[146px]"
+      disabled={!isAgreed}
+      onClick={() => {
+        onConfirm();
+        onOpenChange(false);
+      }}
+    >
+      Batalkan Pesanan
+    </Button>
+  </div>
+);
+
+const CancelContentWhenNotPrepareFleet = ({
+  isAgreed,
+  setIsAgreed,
+  onConfirm,
+  onOpenChange,
+}) => (
+  <div className="relative flex flex-col items-center gap-5">
+    <div className="flex flex-col items-center gap-4 text-center">
+      <h2 className="text-base font-bold text-neutral-900">Batalkan Pesanan</h2>
+      <p className="text-sm font-medium text-neutral-900">
+        Pembatalan pesanan akan dikenakan biaya admin sebesar Rp100.000/unit.
+        <br />
+        <br />
+        Apakah kamu yakin ingin membatalkan pesanan?
+      </p>
+    </div>
+    <Checkbox
+      checked={isAgreed}
+      onChange={({ checked }) => setIsAgreed(checked)}
+      className="w-full"
+    >
+      <span className="text-sm font-semibold text-neutral-900">
+        Saya menyetujui{" "}
+        <Link
+          href="https://faq.muatmuat.com/pusat-bantuan"
+          className="text-primary-700 underline"
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={(e) => e.stopPropagation()}
+        >
+          Syarat dan Ketentuan Muatrans
+        </Link>
+      </span>
+    </Checkbox>
+    <Button
+      variant="muatparts-primary-secondary"
+      className="w-[146px]"
+      disabled={!isAgreed}
+      onClick={() => {
+        onConfirm();
+        onOpenChange(false);
+      }}
+    >
+      Batalkan Pesanan
+    </Button>
+  </div>
+);
