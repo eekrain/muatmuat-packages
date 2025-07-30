893b1c6baa7e4aaf94d47ebab54afbe09e0ec993
Ardian Eka Candra <candra.ardianeka@gmail.com>
2025-07-30 10:45:29 +0700

    fix: Update loading logic and enhance loading store with devtools integration
    - Modified the loading condition in LoadingInteractive component to correctly display loading state based on global loading and translation readiness.
    - Integrated zustand devtools into the loading store for improved debugging and state management.

--- src/components/Loading/LoadingInteractive.jsx
+++ src/components/Loading/LoadingInteractive.jsx
@@ -7,7 +7,7 @@ export default function LoadingInteractive() {
   const isGlobalLoading = useLoadingStore((state) => state.isGlobalLoading);
   const { isTranslationsReady } = useTranslation();
 
-  if (!isGlobalLoading || !isTranslationsReady) {
+  if (isGlobalLoading || !isTranslationsReady) {
     return (
       <div className="fixed left-0 top-0 z-[9999] flex h-screen w-screen items-center justify-center bg-black/10 backdrop-blur-md">
         <img
--- src/store/Shared/loadingStore.js
+++ src/store/Shared/loadingStore.js
@@ -1,11 +1,20 @@
 import { create } from "zustand";
 
-export const useLoadingStore = create((set) => ({
-  isGlobalLoading: true,
-  actions: {
-    setIsGlobalLoading: (isGlobalLoading) => set({ isGlobalLoading }),
-  },
-}));
+import { zustandDevtools } from "@/lib/utils";
+
+export const useLoadingStore = create(
+  zustandDevtools(
+    (set) => ({
+      isGlobalLoading: true,
+      actions: {
+        setIsGlobalLoading: (isGlobalLoading) => set({ isGlobalLoading }),
+      },
+    }),
+    {
+      name: "global-loading-store",
+    }
+  )
+);
 
 export const useLoadingAction = () => {
   const { setIsGlobalLoading } = useLoadingStore((s) => s.actions);
