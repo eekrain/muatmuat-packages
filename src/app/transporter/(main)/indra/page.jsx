9b4b22f1309fc474eb972ef7565faef6e336f419
cakradana <rcakradana@gmail.com>
2025-08-05 18:36:48 +0700

    add PermintaanAngkut component and create page for transporter

---
+++ src/app/transporter/(main)/indra/page.jsx
@@ -0,0 +1,6 @@
+import PermintaanAngkut from "@/container/Transporter/PermintaanAngkut/PermintaanAngkut";
+
+const Page = () => {
+  return <PermintaanAngkut />;
+};
+export default Page;
---
+++ src/container/Transporter/PermintaanAngkut/PermintaanAngkut.jsx
@@ -0,0 +1,4 @@
+const PermintaanAngkut = () => {
+  return <div>PermintaanAngkut</div>;
+};
+export default PermintaanAngkut;
