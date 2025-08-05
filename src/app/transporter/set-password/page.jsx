e65c71ad9b733193e67cda25d98a1dd9dd8dfcf2
afixv <afixv00@gmail.com>
2025-08-05 15:45:29 +0700

    feat(register): add set password page

---
+++ src/app/transporter/set-password/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SetPasswordPage from "@/container/Transporter/Auth/SetPassword/SetPasswordPage";
+
+function Page() {
+  return (
+    <>
+      <SetPasswordPage />
+    </>
+  );
+}
+
+export default Page;
