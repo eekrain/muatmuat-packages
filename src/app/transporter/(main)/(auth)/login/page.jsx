701ca50bbef082b8c200b526120dc2732773884e
Fawwazxdam <fawwazadam1005@gmail.com>
2025-08-06 17:14:57 +0700

    feat: Enhance login page with authentication check and redirect; update DetailPesananScreen with new modals and alerts

--- src/app/transporter/(main)/(auth)/login/page.jsx
+++ src/app/transporter/(main)/(auth)/login/page.jsx
@@ -1,13 +1,27 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import React from "react";
 
 import LoginPage from "@/container/Transporter/Auth/Login/LoginPage";
+import { useAuth } from "@/hooks/use-auth";
 
 function Page() {
-  return (
-    <>
-      <LoginPage />
-    </>
-  );
+  const router = useRouter();
+  const { isLoggedIn } = useAuth();
+  console.log("isAuthenticated:", isLoggedIn);
+
+  React.useEffect(() => {
+    if (isLoggedIn) {
+      router.replace("/dashboard");
+    }
+  }, [isLoggedIn, router]);
+
+  if (isLoggedIn) {
+    return null;
+  }
+
+  return <LoginPage />;
 }
 
 export default Page;
