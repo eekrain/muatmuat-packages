bd827dcac33c57968785242eb663c98b97bb3125
Syahrul Ramadhan <119512208+Syahrulramadhan00@users.noreply.github.com>
2025-08-11 09:16:37 +0700

    add: seperate User Journey from parent

--- src/app/transporter/(main)/dashboard/real-time/page.jsx
+++ src/app/transporter/(main)/dashboard/real-time/page.jsx
@@ -5,10 +5,7 @@ import UserJourney from "@/container/Transporter/Dashboard/UserJourney";
 function Page() {
   return (
     <div className="pt-8">
-      <h1 className="px-6 text-xl font-bold text-neutral-900">
-        Dashboard Analytics
-      </h1>
-      <UserJourney />
+      <UserJourney title="Dashboard Real-time" />
     </div>
   );
 }
