b1ef61871b0f5586c9e25285b46d7f1df1b57327
cakradana <rcakradana@gmail.com>
2025-08-20 13:16:25 +0700

    feat(Monitoring): update order tracking to use orderId and integrate multi-fleet tracking data

--- src/app/transporter/(main)/monitoring/page.jsx
+++ src/app/transporter/(main)/monitoring/page.jsx
@@ -440,7 +440,7 @@ const Page = () => {
                       payload: null,
                     });
                   }}
-                  orderData={selections.selectedOrderForTracking}
+                  orderId={selections.selectedOrderForTracking?.id}
                 />
               ) : (
                 <DaftarArmada
