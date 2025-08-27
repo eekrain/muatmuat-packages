2f01b3c58fd67fb924a8bb97b7463d3d95262494
cakradana <rcakradana@gmail.com>
2025-08-27 16:53:25 +0700

    fix: update location text styling for better readability

--- src/components/Stepper/MuatBongkarStepperWithModal.jsx
+++ src/components/Stepper/MuatBongkarStepperWithModal.jsx
@@ -38,7 +38,7 @@ const LocationItem = ({ location, isLast, appearance }) => (
     {/* Location text */}
     <span
       className={cn(
-        "text-[10px] font-semibold leading-[130%] text-neutral-900",
+        "line-clamp-2 break-before-all text-xxs font-semibold text-neutral-900",
         appearance?.titleClassName
       )}
     >
